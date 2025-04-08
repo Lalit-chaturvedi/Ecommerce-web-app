e2fe27983dae6ebea0fb53622a112414745ead18
Lalit chaturvedi <lalitchaturvedi@Lalits-MacBook-Air.local>
2025-04-08 23:39:06 +0530

    initial commit

---
+++ src/main/java/com/ecommarcce/Ecommerce_web_service/EcommerceWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommarcce.Ecommerce_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommarcce/Ecommerce_web_service/EcommerceWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommarcce.Ecommerce_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
